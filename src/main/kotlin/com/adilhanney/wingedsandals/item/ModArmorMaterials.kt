a913e16a968debe3a197c3c08ec4d82baa0a8bd1
Adil Hanney <adilhanney@disroot.org>
2025-05-03 23:29:15 +0100

    Feat: Add armor material

---
+++ src/main/kotlin/com/adilhanney/wingedsandals/item/ModArmorMaterials.kt
@@ -0,0 +1,47 @@
+package com.adilhanney.wingedsandals.item
+
+import com.adilhanney.wingedsandals.WingedSandals
+import net.minecraft.item.ArmorItem
+import net.minecraft.item.ArmorMaterial
+import net.minecraft.item.Items
+import net.minecraft.recipe.Ingredient
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.Identifier
+import net.minecraft.util.Util
+import java.util.*
+import java.util.function.Consumer
+
+object ModArmorMaterials {
+  val wingedSandalsMaterial = registerWingedSandalsMaterial()
+
+  private fun registerWingedSandalsMaterial() : RegistryEntry<ArmorMaterial> {
+    val id = Identifier.of(WingedSandals.MOD_ID, "winged_sandals_material")
+
+    val defense = Util.make(EnumMap<ArmorItem.Type, Int>(ArmorItem.Type::class.java), Consumer { map ->
+      map.put(ArmorItem.Type.BOOTS, 1)
+      map.put(ArmorItem.Type.LEGGINGS, 3)
+      map.put(ArmorItem.Type.CHESTPLATE, 5)
+      map.put(ArmorItem.Type.HELMET, 2)
+      map.put(ArmorItem.Type.BODY, 7)
+    })
+
+    val enchantability = 25
+    val toughness = 0.0F
+    val knockbackResistance = 0.1F
+
+    val soundEvent = SoundEvents.ITEM_ARMOR_EQUIP_GOLD
+
+    val repairIngredient = { Ingredient.ofItems(Items.GOLD_INGOT, ModItems.wingedSandals) }
+
+    val layers = listOf(ArmorMaterial.Layer(id))
+
+    return Registry.registerReference(
+      Registries.ARMOR_MATERIAL,
+      id,
+      ArmorMaterial(defense, enchantability, soundEvent, repairIngredient, layers, toughness, knockbackResistance)
+    )
+  }
+}
--- src/main/kotlin/com/adilhanney/wingedsandals/item/WingedSandalsItem.kt
+++ src/main/kotlin/com/adilhanney/wingedsandals/item/WingedSandalsItem.kt
@@ -1,7 +1,18 @@
 package com.adilhanney.wingedsandals.item
 
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ArmorItem
-import net.minecraft.item.ArmorMaterials
+import net.minecraft.item.ItemStack
+import net.minecraft.world.World
 
-class WingedSandalsItem(settings: Settings) : ArmorItem(ArmorMaterials.GOLD, Type.BOOTS, settings) {
+class WingedSandalsItem(settings: Settings) : ArmorItem(ModArmorMaterials.wingedSandalsMaterial, Type.BOOTS, settings) {
+
+  @Override
+  override fun inventoryTick(stack: ItemStack?, world: World?, entity: Entity?, slot: Int, selected: Boolean) {
+    if (entity is PlayerEntity) {
+      entity.abilities.allowFlying = true
+    }
+    super.inventoryTick(stack, world, entity, slot, selected)
+  }
 }
