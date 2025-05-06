db89f63a64cedd6e1d5f827d194beca515c9a45f
Adil Hanney <adilhanney@disroot.org>
2025-05-06 05:34:51 +0100

    Fix: Compile armor material for 1.21

--- src/main/kotlin/com/adilhanney/wingedsandals/item/ModArmorMaterials.kt
+++ src/main/kotlin/com/adilhanney/wingedsandals/item/ModArmorMaterials.kt
@@ -18,16 +18,35 @@ object ModArmorMaterials {
 
   //? if >=1.21 {
   /*private fun register(armorMaterial: ArmorMaterial) : RegistryEntry<ArmorMaterial> {
-    val id = Identifier.of(WingedSandals.MOD_ID, armorMaterial.name)
-    return Registry.registerReference(Registries.ARMOR_MATERIAL, id, WingedSandalsArmorMaterial.instance)
+    return Registry.registerReference(Registries.ARMOR_MATERIAL, WingedSandalsArmorMaterial.id, WingedSandalsArmorMaterial.instance)
   }
-  *///? } else {
+  *///?} else {
   private fun register(armorMaterial: ArmorMaterial) : ArmorMaterial = armorMaterial
-  //? }
+  //?}
 }
 
+//? if >=1.21 {
+/*class WingedSandalsArmorMaterial {
+  companion object {
+    private val gold = ArmorMaterials.GOLD.value()
+    private val netherite = ArmorMaterials.NETHERITE.value()
+
+    val id = Identifier.of(WingedSandals.MOD_ID, "winged_sandals")!!
+    val instance = ArmorMaterial(
+      EnumMap(ArmorItem.Type::class.java),
+      gold.enchantability,
+      gold.equipSound,
+      { Ingredient.ofItems(Items.GOLD_INGOT, ModItems.wingedSandals) },
+      listOf(ArmorMaterial.Layer(id)),
+      gold.toughness,
+      netherite.knockbackResistance,
+    )
+  }
+}
+*///?} else {
 class WingedSandalsArmorMaterial : ArmorMaterial {
   companion object {
+    val id = Identifier.of(WingedSandals.MOD_ID, "winged_sandals")!!
     val instance = WingedSandalsArmorMaterial()
   }
 
@@ -36,7 +55,8 @@ class WingedSandalsArmorMaterial : ArmorMaterial {
   override fun getEnchantability(): Int = ArmorMaterials.GOLD.enchantability
   override fun getEquipSound(): SoundEvent? = ArmorMaterials.GOLD.equipSound
   override fun getRepairIngredient(): Ingredient? = Ingredient.ofItems(Items.GOLD_INGOT, ModItems.wingedSandals)
-  override fun getName(): String? = "winged_sandals"
+  override fun getName(): String? = id.path
   override fun getToughness(): Float = ArmorMaterials.GOLD.toughness
   override fun getKnockbackResistance(): Float = ArmorMaterials.NETHERITE.knockbackResistance
 }
+//?}
