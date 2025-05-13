89565de1e195ff8bd08a4fa296ee3d313f7777ea
Adil Hanney <adilhanney@disroot.org>
2025-05-13 11:07:12 +0100

    Fix: Generate item model for 1.21.5
    #1

--- src/main/kotlin/com/adilhanney/wingedsandals/datagen/ModModelProvider.kt
+++ src/main/kotlin/com/adilhanney/wingedsandals/datagen/ModModelProvider.kt
@@ -1,27 +1,24 @@
 package com.adilhanney.wingedsandals.datagen
 
 import com.adilhanney.wingedsandals.item.ModItems
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 //? if >=1.21.4 {
 /*import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider
 import net.minecraft.client.data.BlockStateModelGenerator
 import net.minecraft.client.data.ItemModelGenerator
+import net.minecraft.client.data.Models
 *///?} else {
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
 import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.client.ItemModelGenerator
 import net.minecraft.data.client.Models
 //?}
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 
 class ModModelProvider(output: FabricDataOutput) : FabricModelProvider(output) {
   override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
   }
 
   override fun generateItemModels(generator: ItemModelGenerator) {
-    //? if >=1.21.4 {
-    /*generator.register(ModItems.wingedSandals)
-    *///?} else {
     generator.register(ModItems.wingedSandals, Models.GENERATED)
-    //?}
   }
 }
